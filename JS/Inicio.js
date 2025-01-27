6fd1a94ff825ad975b02b94727ab6fc965faf055
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2025-01-27 17:31:22 -0300

    Foi feito o treinamento inicial de tipo de dados, variáveis, operadores e comentários com o JS

---
+++ JS/Inicio.js
@@ -0,0 +1,26 @@
+var a = "gabriel" 
+a = 3
+const b = 7
+var c = a + b
+
+console.log(c)
+
+let d
+d =78
+e = a + d
+console.log(e)
+
+// Resto da divisão
+
+let f = e % d 
+
+console.log(f)
+
+console.log((a + e)*d)
+
+// é necessáro declarar a variável. Var Let e Const
+
+/*
+Comenttário de bloco
+
+*/ 
--- JS/TiposDados.js
+++
@@ -1,5 +0,0 @@
-a = 3n
-b = 7n
-c = a + b
-
-print(c)
