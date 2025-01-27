34c87457d7961064cbef8177f1f5ee5a8a5ba4ec
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2025-01-27 18:07:30 -0300

    Treinamento da manipulção do Console e window na página web. O Document faz referência dirtea do arquivo HTML.

---
+++ JS/PaginaWeb/script.js
@@ -0,0 +1,7 @@
+let nome = "Gabriel"
+
+console.log(nome)
+
+//Para ver o código JS basta abrir o console da página Web
+
+let confirmacao = window.confirm("Está correto o nome?")
---
+++ JS/PaginaWeb/script2.js
@@ -0,0 +1,9 @@
+var c = 35
+
+console.log(c)
+
+window.alert("Olá Mundo")
+
+nome = window.prompt("Digite seu nome")
+
+console.log(nome)
