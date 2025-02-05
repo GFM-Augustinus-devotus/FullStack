e84da9a345be66a56da60aeb6ad2e4b30c551bbe
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2025-02-05 19:59:55 -0300

    Small Introduction to Data Structure and Array.

---
+++ JS/JS-Parte II/Arrays/Array.js
@@ -0,0 +1,14 @@
+let listaDeComras = []
+
+console.log(listaDeComras)
+
+listaDeComras[0] = "Arroz"
+listaDeComras[1] = "Feijão"
+listaDeComras[2] = 5
+
+console.log(listaDeComras)
+
+listaDeComras[5] = true
+
+console.log(listaDeComras)
+
