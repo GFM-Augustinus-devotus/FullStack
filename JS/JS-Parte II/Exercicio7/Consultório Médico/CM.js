91195310deefc447b99a1c0c9487110a10ef2a67
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2025-02-06 20:15:52 -0300

    Medical Apointment beggining

---
+++ JS/JS-Parte II/Exercicio7/Consultório Médico/CM.js
@@ -0,0 +1,14 @@
+let comando = ""
+
+while(comando !== "sair"){
+    switch(comando){
+        case("Visualizar"):
+        break
+        case("Consultar"):
+        break
+        case("Adicionar"):
+        break
+        case("sair"):
+        break
+    }
+}
