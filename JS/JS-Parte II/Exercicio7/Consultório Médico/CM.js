f90dd3e291501c038c3754060bb965dec9b72b98
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2025-02-08 16:24:37 -0300

    Final Adjust for Medical Center

--- JS/JS-Parte II/Exercicio7/Consultório Médico/CM.js
+++ JS/JS-Parte II/Exercicio7/Consultório Médico/CM.js
@@ -1,43 +1,4 @@
-// let comando = ""
-// let fila = []
-// let paciente = ""
 
-// function menuCentroMedico(comando){
-//     while(comando !== "sair"){
-//         switch(comando){
-//             case("Visualizar"):
-//             window.alert("Pacientes na fila: " + fila)
-//             break
-//             case("Consultar"):
-//             paciente = fila.pop()
-//             window.alert("O paciente " + paciente + " foi para o consultório! ")
-//             break
-//             case("Adicionar"):
-//             fila.push(window.prompt("Digite o nome do padiente: "))
-//             break
-//             case("sair"):
-//             break
-//         }
-//     }
-// }
-
-// document.addEventListener("DOMContentLoaded" , function(){
-//     let Visualizar = document.getElementById("Visualizar")
-//     let Consultar = document.getElementById("Consultar")
-//     let Adicionar = document.getElementById("Adicionar")
-//     let Sair = document.getElementById("sair")
-
-//     if (Visualizar === true){
-//         menuCentroMedico("Visualizar")
-    
-//     }else if(Consultar === true){
-//         menuCentroMedico("Consultar")
-//     }else if(Adicionar === true){
-//         menuCentroMedico("Adicionar")
-//     }else if (Sair === true){
-//         menuCentroMedico("sair")
-//     }
-// })
 
 let comando = "";
 let fila = [];
