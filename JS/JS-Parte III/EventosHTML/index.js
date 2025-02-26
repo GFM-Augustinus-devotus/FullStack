22bf75f3c5e60ec60b378089eb9060c55b4cd40a
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2025-02-26 17:31:56 -0300

    Testing some events of elements of HTML and how the event use the JS arquive

---
+++ JS/JS-Parte III/EventosHTML/index.js
@@ -0,0 +1,19 @@
+function onMouseEnter(){
+    console.log("O input foi selecionado")
+}
+
+function onChange(){
+    console.log("Ocorreu uma mudança de valor")
+}
+
+function onFoucus(){
+    console.log("Elemento está com foco")
+}
+
+function onBlur(){
+    console.log("Elemento perdeu o foco")
+}
+
+function onClick(){
+    console.log("O botão confirmar foi clicado")
+}
