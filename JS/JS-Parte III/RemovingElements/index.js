ba75c301cdb0588f20623dce93262bafd41844d8
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2025-03-01 18:38:37 -0300

    Finishing The removing Contacts Method

--- JS/JS-Parte III/RemovingElements/index.js
+++ JS/JS-Parte III/RemovingElements/index.js
@@ -1,7 +1,7 @@
 //Creatin new elements
 
 function addContact(){
-    const contactSection = document.getElementById('contact-list')
+    const contactSection = document.getElementById('contacts-list')
     const h3 = document.createElement('h3')
     const ul = document.createElement('ul')
     const nameLi = document.createElement('li')
@@ -18,4 +18,22 @@ function addContact(){
     ul.appendChild(document.createElement('br'))
 
     //Creatting Contacts Phones
+    const phoneli = document.createElement('li')
+    const phoneInput = document.createElement('input')
+    phoneli.innerText = 'phone: '
+    phoneInput.type = 'text'
+    phoneInput.name = 'phone'
+    phoneli.appendChild(phoneInput)
+    ul.appendChild(phoneli) 
+    ul.appendChild(document.createElement('br'))
+
+    contactSection.append(h3 , ul)
+}
+
+function removeContact(){
+    const cantactSection = document.getElementById('contacts-list')
+    const titles = document.getElementsByTagName('h3')  // Get all the titles
+    const contacts = document.getElementsByTagName('ul')   // Get all the contacts
+    cantactSection.removeChild(titles[titles.length - 1])   // Remove the last contact
+    cantactSection.removeChild(contacts[contacts.length - 1])
 }
