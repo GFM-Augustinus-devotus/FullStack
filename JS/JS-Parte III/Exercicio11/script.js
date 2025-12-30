be3df0a197fe00dee4fbababc42628cd71a65173
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2025-12-30 11:17:05 -0300

    Working with DOM

--- JS/JS-Parte III/Exercicio11/script.js
+++ JS/JS-Parte III/Exercicio11/script.js
@@ -5,7 +5,7 @@ function cadastrar(){
     const posicaoEl = document.getElementById("posicao")
     const camisaEl = document.getElementById("camisa")
 
-    const nome = nomeEl.value.trim()
+    const nome = nomeEl.value.trim() // trim() retira os espaços vazios 
     const posicao = posicaoEl.value.trim()
     const camisa = camisaEl.value.trim()
 
@@ -14,24 +14,35 @@ function cadastrar(){
         return
     }
 
-    // evita número de camisa duplicado
-    const exists = Array.from(lista.querySelectorAll('li')).some(li => li.dataset.camisa === camisa)
-    if (exists) {
-        alert('Número de camisa já está escalado.');
-        return
+    const confirmation =  confirm("Escalar " + nome + " como " + posicao + "?" )
+    
+    if (confirmation) {
+        
+        // evita número de camisa duplicado
+        const exists = Array.from(lista.querySelectorAll('li')).some(li => li.dataset.camisa === camisa)
+
+        if (exists) {
+            alert('Número de camisa já está escalado.');
+            return
+        }
+
+        const li = document.createElement('li')
+        li.dataset.camisa = camisa
+        //Cada jogador vai ter um id de acordo com o valor do número da camisa
+        li.id = 'jogador-'+camisa
+
+        // formato simples: Posição - Nome (Camisa N)
+        li.textContent = `${posicao} - ${nome} ( ${camisa} )`
+
+        lista.appendChild(li)
+
+        // limpa campos
+        nomeEl.value = ''
+        posicaoEl.value = ''
+        camisaEl.value = ''
     }
 
-    const li = document.createElement('li')
-    li.dataset.camisa = camisa
-    // formato simples: Posição - Nome (Camisa N)
-    li.textContent = `${posicao} - ${nome} (Camisa ${camisa})`
 
-    lista.appendChild(li)
-
-    // limpa campos
-    nomeEl.value = ''
-    posicaoEl.value = ''
-    camisaEl.value = ''
 }
 
 function remover(){
@@ -41,15 +52,21 @@ function remover(){
     if (!camisaRemover) {
         alert('Informe o número da camisa a remover.')
         return
+    }    
+
+    const confirmation =  confirm("Remover o jogador de camisa " + camisaRemover + "?")
+
+    if (confirmation){
+
+        const li = Array.from(lista.querySelectorAll('li')).find(li => li.dataset.camisa === camisaRemover)
+
+        if (!li) {
+            alert('Jogador com esse número não encontrado.')
+            return
+        }
+
+        lista.removeChild(li)
+        document.getElementById("camisa_remover").value = ''
     }
 
-    const li = Array.from(lista.querySelectorAll('li')).find(li => li.dataset.camisa === camisaRemover)
-
-    if (!li) {
-        alert('Jogador com esse número não encontrado.')
-        return
-    }
-
-    lista.removeChild(li)
-    document.getElementById("camisa_remover").value = ''
 }
