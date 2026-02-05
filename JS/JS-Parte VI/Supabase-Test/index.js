36d89b0ec7098380dc02101175658134e7adea3b
GFM-Austinus-devotus <gfreitasmelo67@gmail.com>
2026-02-05 16:52:05 -0300

    Remaking Supabase conections

--- JS/JS-Parte VI/Supabase-Test/index.js
+++ JS/JS-Parte VI/Supabase-Test/index.js
@@ -1,23 +1,7 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
-
-// Coloquei as informações do cadastro no Supabase
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseURL = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
 const supabase = createClient(supabaseURL, supabaseAnonKey)
 
-async function getTables() {
-    const { data, error } = await supabase.from('information_schema.tables').select('table_name').eq('table_schema', 'public')
-    if (error) {
-        console.error('Error fetching tables:', error)
-    } else {
-        data.forEach(table => {
-            const p = document.createElement('p')
-            p.textContent = `Schema: public, Table: ${table.table_name}`
-            document.querySelector('#db-system').appendChild(p)
-        })
-    }
-}
-
-getTables()
+console.log(supabase)
