938335966cab97afa6b85dc6fda3eb5a3312619d
GFM-Austinus-devotus <gfreitasmelo67@gmail.com>
2026-02-18 17:08:42 -0300

    Suoabase Conection Using Vite Server

--- JS/JS-Parte VI/Supabase-Test/index.js
+++ JS/JS-Parte VI/Supabase-Test/index.js
@@ -1,19 +1,39 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-const supabase = createClient(supabaseURL, supabaseAnonKey)
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
-const { data, error } = await supabase.from('First_test').select('*')
+const { data, error } = await supabase.auth.signUp({
+  email: "gfreitasmelo67@gmail.com.com",
+  password: "@GestaoDev2026",
+});
 
-if (error) {
-    console.error('Error fetching data:', error)
-    // Populate the main element with error
-    const main = document.getElementById('db-system')
-    main.innerHTML = '<h2>Error:</h2><pre>' + JSON.stringify(error, null, 2) + '</pre>'
-} else {
-    console.log('Data:', data)
-    // Populate the main element
-    const main = document.getElementById('db-system')
-    main.innerHTML = '<h2>Data from Supabase:</h2><pre>' + JSON.stringify(data, null, 2) + '</pre>'
-}
+console.log({ data, error });
+
+
+
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// const supabase = createClient(supabaseURL, supabaseAnonKey);
+
+// const main = document.getElementById("db-system");
+// main.innerHTML = "<p>Carregando…</p>";
+
+// const { data, error } = await supabase
+//     .schema("cem")
+//     .from("secretarias")
+//     .select("*");
+
+// if (error) {
+//   console.error(error);
+//   main.innerHTML =
+//     "<h2>Error</h2><pre>" + JSON.stringify(error, null, 2) + "</pre>";
+// } else {
+//   main.innerHTML =
+//     "<h2>Dados</h2><pre>" + JSON.stringify(data, null, 2) + "</pre>";
+// }
