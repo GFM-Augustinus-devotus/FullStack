4b5712bbe9ad7f471f132531ac01cc77ed553ba3
GFM-Austinus-devotus <gfreitasmelo67@gmail.com>
2026-02-26 14:33:30 -0300

    Using While to control car velocity

---
+++ JS/Review-JS/Loops/while.js
@@ -0,0 +1,10 @@
+let velocidade = 100
+
+while(velocidade >= 0){
+    alert("Velocidade Atual "+ velocidade)
+    velocidade -= 20
+        if(velocidade == 0){
+        alert("O carro parou")
+        break
+    }
+}
