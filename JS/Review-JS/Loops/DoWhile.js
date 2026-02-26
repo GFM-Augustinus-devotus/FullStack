5cc37cff4a55b1c476c21c95b6cd740a59de462c
GFM-Austinus-devotus <gfreitasmelo67@gmail.com>
2026-02-26 15:27:05 -0300

    Do While Struct

---
+++ JS/Review-JS/Loops/DoWhile.js
@@ -0,0 +1,8 @@
+let velocidade = 0
+
+do{
+    alert("Velocidade do verículo é "+ velocidade)
+    velocidade += 20
+}while(velocidade < 130)
+
+alert("Velocidade Final é : "+ velocidade + " você passou do limite da velocidade")
