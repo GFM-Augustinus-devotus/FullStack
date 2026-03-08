1c78b9073800ab7f0a48de0bf087ecca4e71a429
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2026-03-08 12:26:20 -0300

    Pilha de cartas está completa

--- JS/Review-JS/Ex7/pilhaCartas.js
+++ JS/Review-JS/Ex7/pilhaCartas.js
@@ -1,16 +1,46 @@
 let baralho = ["4 paus", "7 copas", "A espadas", "7 ouros"]
-const pilha = document.getElementById('baralho')
-const nomeCarta = document.getElementById("nomeCarta")
-const naipeCarta = document.getElementById("naipeCarta")
 
-function mostrarBaralho(){
-    for(let i = baralho.length; i >= 0; i--){
-        const newLi = document.createElement('li')
-        newLi.className = "Cartas do baralho"
-        newLi.id = i
-        newLi.innerText = baralho[1]
-        pilha.append(newLi)
+// No cado do DOM contentLoaded serve para garantir que os elementos estejam disponíveis antes de acessá-los
+window.addEventListener('DOMContentLoaded', function() {
+    const pilha = document.getElementById('baralho')
+    const nomeCarta = document.getElementById("nomeCarta")
+    const naipeCarta = document.getElementById("naipeCarta")
+
+    function mostrarBaralho(){
+        pilha.innerHTML = '';
+        for(let i = baralho.length-1 ; i >= 0; i--){
+            const newLi = document.createElement('li')
+            newLi.innerText = baralho[i]
+            pilha.append(newLi)
+        }
     }
-}
 
-mostrarBaralho()
+    function adicionar(){
+        const nome = nomeCarta.value;
+        const naipe = naipeCarta.value;
+        if (nome && naipe) {
+            const carta = nome + " " + naipe;
+            baralho.push(carta);
+            mostrarBaralho();
+        }else{
+            alert("Insira uma carta corretamente")
+        }
+    }
+
+    function remover(){
+        if(baralho.length>0){
+            baralho.pop()
+            mostrarBaralho()
+        }else{
+            alert("O baralho está vazio")
+        }
+    }
+
+
+
+    window.adicionar = adicionar;
+    window.remover = remover;
+
+    mostrarBaralho();
+
+});
