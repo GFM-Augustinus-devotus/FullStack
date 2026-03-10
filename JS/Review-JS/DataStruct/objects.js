31b59f4884a1836feabaec7b3490667123be63d4
GFM-Austinus-devotus <gfreitasmelo67@gmail.com>
2026-03-10 15:07:12 -0300

    Working with objects

---
+++ JS/Review-JS/DataStruct/objects.js
@@ -0,0 +1,40 @@
+//Objetos é uma estrutura de chave-valor (propriedade)
+//Chave é nome da propriedade e o valor é o resultado dessa propriedade
+// Seus elementos não possuem uma sequência
+
+let user = {
+    nome: "Gabriel",
+    idade: 14,
+    profissão: "Programador",
+    formacaoSuperior:  true,
+    endereco: {
+        rua: "Guajá",
+        numero: 1,
+        bairro: "Jardim Atlântico"
+    }
+}
+
+let aluno = {
+    prop: "Olá mundo"
+}
+
+
+console.log(user.nome)
+console.log(aluno["Olá mundo"])
+
+//----- Referenciando funções e propriedades dentro de objetos. Base do POO
+
+let funcao = "log"
+
+console[funcao]("Olá, bom dia")
+
+// Chamando um objeto como todo. Em vez de passar uma variável por vez, passa todo objeto
+
+console.log(user)
+
+user.colegas = ["Maria, José, Sebastião"]
+
+// Pode formar objetos dentro de objetos
+
+
+
