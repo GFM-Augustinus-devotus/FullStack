112cec2cad92b20208a37fb847d30d3d7526800c
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2026-05-03 17:31:15 -0300

    Changing the attributes of document elements

---
+++ JS/JS-Parte III/changeAttributes/script.js
@@ -0,0 +1,31 @@
+// Podemos Manipular qualquer atributo do HTML com o JS
+const input = document.getElementById('input')
+
+document.getElementById('value').addEventListener('click', function(){
+    input.value = "Olá mundo!"  //Operador Ternário
+
+    console.log(input.value) //Comportamento dinâmico, valor em tempo real
+    console.log(input.getAttribute('value')) // Pega o valor pré definido
+})
+
+document.getElementById('type').addEventListener('click', function(){
+    //input.type = input.type !== 'radio' ? 'radio' : 'text'
+    input.setAttribute('type', 'radio')
+})
+
+document.getElementById('placeholder').addEventListener('click', function(){
+    input.placeholder = 'digite algo'
+})
+
+document.getElementById('disabled').addEventListener('click', function(){
+    input.setAttribute('disabled', !input.disabled)
+})
+
+//Atributos data : (Armazenar valores customisáveis pelo HTML). Você os chame por meio do atributo dataset
+
+document.getElementById('data').addEventListener('click', function(){
+    const data = input.dataset.something
+    console.log("O valor de data something é " + data)
+    input.dataset.something = "Segundo valor setado"
+    console.log("O valor de data something é " + data)
+})
