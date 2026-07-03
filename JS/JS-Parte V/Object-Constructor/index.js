a8c5e4bdec340faa0e7f42ef01d57c31743ca24c
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2026-07-03 15:03:16 -0300

    Startind the POO

---
+++ JS/JS-Parte V/Object-Constructor/index.js
@@ -0,0 +1,20 @@
+// Constructor of objects
+function Car(name, brand, year, type, price){
+    this.name = name,
+    this.brand = brand,
+    this.year = year,
+    this.type = type,
+    this.price = price
+    this.available = false
+    this.inStock = 0
+    this.addStock = function addOnStock(quantity){
+        this.inStock += quantity
+    }
+    this.save = function(){
+        // Save the new object
+    }
+}
+
+const jetta = new Car("Jetta", "VolksWagen", 2012, "Sedan", 58900.00)
+
+console.log(jetta)
