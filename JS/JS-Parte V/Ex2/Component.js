de915879a01edbccd0767ee31b3c68a576061a95
GFM-Augustinus-devotus <gfreitasmelo67@gmail.com>
2026-07-14 16:56:35 -0300

    Dom with Classes

--- JS/JS-Parte V/Ex2/Component.js
+++ JS/JS-Parte V/Ex2/Component.js
@@ -1,13 +1,21 @@
 // Faça primeiro para Componente e tente Exportar ele para index.js e ver suas inteções na página HTML
 export {Component}
 
+//A classe Componente irá nos ajudar a trabalhar com o DOM
+
 class Component {
     
-    #refElement // Armazena o tipo do Componente. Label, input, br ...
+    #refElement  = null// Armazena o tipo do Componente. Label, input, br ...
 
-    constructor(element){
-        this.#refElement = element 
+    constructor(tag, parent, options){ //Options se referem aos atributos do HTML
+        this.tag = tag
+        this.parent = parent
+        this.options = options //As options de uma Tag são os atributos de um objeto
     }
+    build(){ //Método para criar um elemento (Público)
+        this.#refElement = document.createElement(this.tag)
+    }
+
     getRefElement(){ // Método para acessar atributo privado
         return JSON.stringify(this.#refElement)
     }
